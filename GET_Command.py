e6a9e631816e5632082d654f4b8de48bc3cb6f47
Jasio10 <jasio10@yahoo.com>
2022-01-05 21:48:05 -0600

    HTTP Client

--- GET_Command.py
+++ GET_Command.py
@@ -1,6 +1,4 @@
 # Create basic HTTP Client
 import requests
-response = requests.get("http://api.open-notify.org/astros.json")
-# response = requests.get("http://www.booking.com/")
+response = requests.get("http://www.booking.com/")
 print(response)
-print(response.json())
