05b438a423598cc3db1233c8fc3fb7fa7f3e9047
Jasio10 <jasio10@yahoo.com>
2022-01-10 22:03:16 -0600

    square

---
+++ square.py
@@ -0,0 +1,3 @@
+# We have a set of points described by (x, y) coordinates in the two dimensional space.
+# Create a function, that calculates coordinates of smallest possible square (coord and size) that fits all of them.
+# Two sides of the square are expected to be parallel to the x axis
