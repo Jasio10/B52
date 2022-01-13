f29b9fce27195dfdd943b0775ee33a7189695aad
Jasio10 <jasio10@yahoo.com>
2022-01-12 19:01:33 -0600

    square and input

--- square.py
+++ square.py
@@ -1,47 +1,61 @@
 # We have a set of points described by (x, y) coordinates in the two dimensional space.
-# Create a function, that calculates coordinates of smallest possible square (coord and size) that fits all of them.
-# Two sides of the square are expected to be parallel to the x axis
+# Create a function, that calculates coordinates of the smallest possible square (coord and size) that fits all of them.
+# Two sides of the square are expected to be parallel to the x-axis
+# Program prints results on console and draws points and square in a separate window
 
-import numpy as np
+import json
+import argparse
+import matplotlib.pyplot as plt
 
-# N = 5
-# x = np.random.rand(N)
-# y = np.random.randint(N)
-# x=100*x
+# Uploading points from JSON file
+parser = argparse.ArgumentParser()
+parser.add_argument("-f", "--filename", required=True, type=str)
+args = parser.parse_args()
+with open(args.filename) as json_data:
+    points = json.load(json_data)
+    print("Points from file : ", points)
 
-# print(x," ")
+# Calculating smallest possible square that fits all points
 xCo = []
 yCo = []
-a=6
-b=6
-points = [(4, 5), (8, 2),(1, 2), (3, 3)]
-points.append((6,6))
-print(points)
 for x in points:
     xCo.append(x[0])
     yCo.append(x[1])
-print(xCo)
-print(yCo)
-
 xCo.sort()
 yCo.sort()
-print(xCo)
-print(yCo)
-
 xDist = xCo[-1] - xCo[0]
-print("X longest dist : ", xDist)
 yDist = yCo[-1] - yCo[0]
-print("Y longest dist : ", yDist)
 
 if xDist > yDist:
-    print("Square size is : ", xDist, " x ", xDist)
-else:print("Square size is : ", yDist, " x ", yDist)
-# x=points[1]
-# x1=x[0]
-# print(x)
-# print(x1)
-import matplotlib.pyplot as plt
+    Dist = xDist
+    print("\nSquare size is : ", Dist, " x ", Dist)
+else:
+    Dist = yDist
+    print("Smallest square size is : ", Dist, " x ", Dist)
+
+# Generating corners of the smallest square
+firstPt = (xCo[0], yCo[0])
+secondPt = (firstPt[0]+Dist, firstPt[1])
+thirdPt = (secondPt[0], secondPt[1]+Dist)
+fourthPt = (thirdPt[0]-Dist, thirdPt[1])
+squarePt = []
+squarePt.append(firstPt)
+squarePt.append(secondPt)
+squarePt.append(thirdPt)
+squarePt.append(fourthPt)
+print("Four corners of the smallest square : ", squarePt)
+
+# Drawing square and points
+xCor = []
+yCor = []
+for x in squarePt:
+    xCor.append(x[0])
+    yCor.append(x[1])
+xCor.append(firstPt[0])
+yCor.append(firstPt[1])
+plt.title("Smallest possible square that fits all points")
+plt.plot(xCor, yCor)
 for x in points:
-    c,d= zip(x)
-    plt.plot(c,d, marker="o", color="red")
+    c, d = zip(x)
+    plt.plot(c, d, marker="o", color="red")
 plt.show()
